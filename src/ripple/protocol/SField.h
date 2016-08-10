233ce382105b63c8bd4ed35a27755210cb233f91
wilsonianb <bwilson@ripple.com>
2016-08-09 22:42:43 -0700

    Make payment channels bidirectional:
    Bidirectional payment channels allow two account holders to make
    off-ledger XRP payments with one another without having to trust each
    other. Payments can be sent indefinitely so long as the net total does
    not exceed the paying party's channel balance. Both parties can fund
    the channel to increase their balance.
    
    When authorizing a channel payment, the total amount of XRP paid in the
    channel by the authorizer is signed (not just the amount for the latest
    payment). This amount can exceed the authorizer's channel balance if
    the counterparty has authorized a sufficient amount in the other
    direction. The amounts authorized by each party are kept separate to
    allow channel payments to be sent in a desynchronized manner (parties
    can send payments independently of one another). Payment amounts can be
    netted by subtracting both by the smaller amount and incrementing the
    sequence number on the authorized payments. This can act to obscure the
    channel's total payment volume.

--- src/ripple/protocol/SField.h
+++ src/ripple/protocol/SField.h
@@ -448,6 +448,8 @@ extern SF_Blob const sfCreateCode;
 extern SF_Blob const sfMemoType;
 extern SF_Blob const sfMemoData;
 extern SF_Blob const sfMemoFormat;
+extern SF_Blob const sfMasterSignature;
+extern SF_Blob const sfDstPublicKey;
 
 // variable length (uncommon)
 extern SF_Blob const sfFulfillment;
@@ -484,6 +486,8 @@ extern SField const sfMemo;
 extern SField const sfSignerEntry;
 extern SField const sfSigner;
 extern SField const sfMajority;
+extern SField const sfChannelMember;
+extern SField const sfChannelClaim;
 
 // array of objects
 // ARRAY/1 is reserved for end of array
@@ -496,6 +500,8 @@ extern SField const sfSufficient;
 extern SField const sfAffectedNodes;
 extern SField const sfMemos;
 extern SField const sfMajorities;
+extern SField const sfChannelMembers;
+extern SField const sfChannelClaims;
 
 //------------------------------------------------------------------------------
 
